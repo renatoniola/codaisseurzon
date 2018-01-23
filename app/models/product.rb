542941d70b3eaf95cf9a6800e9484b81ecacf7ec
renato niola <niolarenato01@gmail.com>
2018-01-23 14:05:45 +0100

    fix product model tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
   has_many :carts, dependent: :destroy
   has_many :users, through: :carts
+
+  validates :name , presence: true
+  validates :name , length: { maximum: 50 }
+  validates :name , length: { minimum: 5 }
+  validates :price , presence: true
+  validates :name , uniqueness: true
+
 end
