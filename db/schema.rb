b6804bd79a9440a0670bbbd575d15e362d348645
Felipe Menegossi <felipe.menegossi@gmail.com>
2018-01-26 13:33:16 +0100

    Drop carts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180123140348) do
+ActiveRecord::Schema.define(version: 20180126123014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "carts", force: :cascade do |t|
-    t.bigint "user_id"
-    t.decimal "total"
-    t.integer "count"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "product_id"
-    t.index ["product_id"], name: "index_carts_on_product_id"
-    t.index ["user_id"], name: "index_carts_on_user_id"
-  end
 
   create_table "checkouts", force: :cascade do |t|
     t.string "token"
@@ -84,8 +73,6 @@ ActiveRecord::Schema.define(version: 20180123140348) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "carts", "products"
-  add_foreign_key "carts", "users"
   add_foreign_key "checkouts", "products"
   add_foreign_key "checkouts", "users"
   add_foreign_key "photos", "products"
